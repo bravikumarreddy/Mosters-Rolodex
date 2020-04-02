993de098b39a0e40ac8a9e027f7a685b2b0bb5d7
System Administrator <root@Ravis-MacBook-Pro.local>
2020-04-02 23:08:03 +0530

    Updates

---
+++ precache-manifest.6540a7b7f5ff86420a137b86c666ccdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6d03548220e44fc3080315d52c59ca",
+    "url": "/Mosters-Rolodex/index.html"
+  },
+  {
+    "revision": "22a972c638452c86a5ac",
+    "url": "/Mosters-Rolodex/static/css/main.29985a6a.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/Mosters-Rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mosters-Rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a972c638452c86a5ac",
+    "url": "/Mosters-Rolodex/static/js/main.68ea1a35.chunk.js"
+  },
+  {
+    "revision": "9fb570677236d036edbc",
+    "url": "/Mosters-Rolodex/static/js/runtime-main.5f862a72.js"
+  }
+]);
